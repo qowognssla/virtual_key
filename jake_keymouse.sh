0207927d4594ad84b6b1763c9a995f17f4c45f4e
jake.bae <jake.bae@realtek.com>
2020-08-21 12:22:14 +0900

    update mouse function and rename the file to jake_keymouse.sh

--- jake_keymouse.sh
+++ jake_keymouse.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+X=0
+Y=0
 
 read -p "Please select the input event (/dev/input/event%d) : " inputEvent
-
 while IFS= read -rsn1 k # Read one key (first byte in key press)
 do
     case "$k" in
@@ -32,12 +33,16 @@ do
         k=10001
 	    echo Tab pressed!
 	    ;;
+    $'\1') # mouse Enter
+        echo mouse click!
+        luna-send -n 1 palm://com.lge.inputgenerator/pushMouseEvent '{"eventtype":"click"}'
+	    ;;
     $'\x1b') # ESC
+        echo "esc!"
         read -rsn1 k
         [ "$k" == $'\x1b' ] && echo "esc!!" && k=27     # Esc-Key
         [ "$k" == "[" ] && read -rsn1 k
         [ "$k" == "O" ] && read -rsn1 k
-        echo "$k"
         case "$k" in
         A) # Up
             k=27917965
@@ -55,8 +60,34 @@ do
             k=27917969
             ;;
         1) #Home for windows
-            read -rsn1 k
-            k=27917969
+            read -rsn1 k    
+            if [ $k == ';' ]; then
+                read -rsn1 k
+                if [ $k == '5' ]; then
+                    read -rsn1 k
+                    case "$k" in
+                    A) # Up      
+                        if [ $X != 0 ]; then
+                            X="$(($X-15))"
+                        fi
+                        ;;
+                    B) # Down
+                        X="$(($X+15))"    
+                        ;;
+                    C) # Right
+                        Y="$(($Y+15))"
+                        ;;
+                    D) #Left
+                        if [ $Y != 0 ]; then
+                            Y="$(($Y-15))"
+                        fi
+                        ;;
+                    esac
+                    luna-send -n 1 palm://com.lge.inputgenerator/pushMouseEvent '{"eventtype": "setpos","x": "'$Y'", "y": "'$X'"}'
+                fi
+            else
+                k=27917969
+            fi
             ;;
         2) #Insert
             read -rsn1 k
@@ -68,15 +99,13 @@ do
             ;;
         5) #Page-Up
             read -rsn1 k
+                echo Page UP
             k=27917973
             ;;
         6) #Page-Down
             read -rsn1 k
             k=27917974
             ;;
-        #F) #End
-        #    k=27917975
-        #    ;;
         P) #F1
             k=27917976
             ;;
@@ -84,8 +113,6 @@ do
             k=27917977
             ;;
         esac
-        #read -rsn4 -t .1 # Try to flush out other sequences ...
-    
     esac
     ./virtual_keyboard $inputEvent $k
 done
